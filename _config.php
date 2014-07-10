71e250ec1d4cbecd0e3ea834d0831037ff3d89d1
Igor Nadj <igor@silverstripe.com>
2014-07-10 17:20:01 +1200

    registering error logger

--- _config.php
+++ _config.php
@@ -3,3 +3,5 @@
 DevelopmentAdmin::registerAction('ErrorLogsController', 'logs', 'Error logs');
 DevelopmentAdmin::registerAction('DevToolsController', 'tools', 'Tools');
 
+// log errors and warnings
+SS_Log::add_writer(new DataObjectErrorWriter(), SS_Log::WARN, '<=');
